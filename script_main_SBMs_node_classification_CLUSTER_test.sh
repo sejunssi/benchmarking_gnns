5c2662d08bb21f93df9849702c906c7384a35f2d
sejunssi <skim3393@gmail.com>
2020-04-13 01:27:20 -0500

    as

--- script_main_SBMs_node_classification_CLUSTER_test.sh
+++ script_main_SBMs_node_classification_CLUSTER_test.sh
@@ -31,25 +31,71 @@
 ############
 # SBM_CLUSTER - 4 RUNS  
 ############
+#
+#seed0=41
+#seed1=95
+#seed2=12
+#seed3=35
+#code=main_SBMs_node_classification.py
+#tmux new -s benchmark_SBMs_node_classification -d
+#tmux send-keys "source activate benchmark_gnn" C-m
+#dataset=SBM_CLUSTER
+#tmux send-keys "
+#python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/SBMs_node_clustering_GCN_CLUSTER.json'
+#wait" C-m
+#tmux send-keys "
+#python $code --dataset $dataset --gpu_id 1 --seed $seed0 --config 'configs/SBMs_node_clustering_GraphSage_CLUSTER.json'
+#wait" C-m
+#tmux send-keys "
+#python $code --dataset $dataset --gpu_id 2 --seed $seed0 --config 'configs/SBMs_node_clustering_GAT_CLUSTER.json'
+#wait" C-m
+#tmux send-keys "tmux kill-session -t benchmark_SBMs_node_classification" C-m
+
+
+
+
+
+############
+# SBM_CLUSTER - 4 RUNS
+############
 
 seed0=41
 seed1=95
 seed2=12
 seed3=35
 code=main_SBMs_node_classification.py
-tmux new -s benchmark_SBMs_node_classification -d
+tmux new -s benchmark_SBMs_node_cluster -d
 tmux send-keys "source activate benchmark_gnn" C-m
-dataset=SBM_CLUSTER
-tmux send-keys "
-python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/SBMs_node_clustering_GCN_CLUSTER.json'
-wait" C-m
-tmux send-keys "
-python $code --dataset $dataset --gpu_id 1 --seed $seed0 --config 'configs/SBMs_node_clustering_GraphSage_CLUSTER.json'
-wait" C-m
-tmux send-keys "
-python $code --dataset $dataset --gpu_id 2 --seed $seed0 --config 'configs/SBMs_node_clustering_GAT_CLUSTER.json'
-wait" C-m
-tmux send-keys "tmux kill-session -t benchmark_SBMs_node_classification" C-m
+
+for a in 1 2 3 4 8
+do
+  dataset=SBM_CLUSTER_a${a}
+  printf $dataset
+  tmux send-keys "
+
+  wait" C-m
+done
+tmux send-keys "tmux kill-session -t benchmark_SBMs_node_cluster" C-m
+
+
+#!/bin/bash
+#python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/SBMs_node_clustering_MLP_CLUSTER_a${a}.json' &
+# check :
+# bash script.sh
+# tmux attach -t script_mol_opt
+# tmux detach
+# pkill python
+
+# bash script_main_SBMs_node_classification_PATTERN.sh
+
+
+
+
+
+
+
+
+
 
 
 
