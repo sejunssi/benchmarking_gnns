312c3acfeb96a801541ebb44fe9dbdd5e952e92e
sejunssi <skim3393@gmail.com>
2020-04-13 10:22:53 -0500

    fix test

--- nets/SBMs_node_classification/gin_net.py
+++ nets/SBMs_node_classification/gin_net.py
@@ -73,9 +73,9 @@ class GINNet(nn.Module):
 
         # calculating label weights for weighted loss computation
         V = label.size(0)
-        #label_count = torch.bincount(label)
-        label_unique = label.unique(sorted=True)
-        label_count = torch.stack((label==l_u).sum() for l_u in label_unique)
+        label_count = torch.bincount(label)
+        # label_unique = label.unique(sorted=True)
+        # label_count = torch.stack((label==l_u).sum() for l_u in label_unique)
 
         label_count = label_count[label_count.nonzero()].squeeze()
         cluster_sizes = torch.zeros(self.n_classes).long().to(self.device)
