94549464d2206568164a0ff95137e7624079aaa8
sejunssi <skim3393@gmail.com>
2020-04-13 01:03:48 -0500

    fix smoothed_data

--- fix_smoothed_data.py
+++ fix_smoothed_data.py
@@ -1,20 +1,22 @@
 import pickle
+import torch
 
 class DotDict(dict):
     def __init__(self, **kwds):
         self.update(kwds)
         self.__dict__ = self
 
-NAME = ['SBM_CLUSTER',  'SBM_PATTERN']
+NAME = ['SBM_CLUSTER', 'SBM_PATTERN']
 a_list = [1,2,3,4,8]
 
 for name in NAME:
-    for a in a_list
+    for a in a_list:
+        print(a)
         with open(f'{name}_a{a}.pkl', 'rb') as f:
             data = pickle.load(f)
             train = data[0]
-            data[0]['node_label'] = []
-            for data in data.dataset:
-                data[0]['node_label'].extend(data['node_label'])
-            with open(f'./smoothed_data/{name}_a{a}.pkl', 'w') as f:
+            data[0].node_labels = []
+            for x in train.dataset:
+                data[0].node_labels.append(x['node_label'])
+            with open(f'./smoothed_data/{name}_a{a}.pkl', 'wb') as f:
                 pickle.dump(data, f)
