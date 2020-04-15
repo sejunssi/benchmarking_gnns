b0ccefa7034a2025277556a9bcc28b64eec0eb26
Yonsei Big Data Analytics Laboratory <sejunssi>
2020-04-15 05:12:54 +0000

    fix script

--- script_main_SBMs_node_classification_ALL.sh
+++ script_main_SBMs_node_classification_ALL.sh
@@ -7,8 +7,8 @@ seed0=41
 seed1=95
 seed2=12
 seed3=35
-#code=main_SBMs_node_classification.py
-#tmux new -s benchmark_SBMs_node_classification -d
+code=main_SBMs_node_classification.py
+tmux new -s benchmark_SBMs_node_classification -d
 #tmux send-keys "source activate benchmark_gnn" C-m
 #
 #dataset=SBM_CLUSTER_smoothing
