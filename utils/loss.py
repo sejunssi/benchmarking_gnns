ee0dd24b51fa469216bae5de3ac38c408297df08
sejunssi <skim3393@gmail.com>
2020-04-16 22:23:35 -0500

    fix loss

--- utils/loss.py
+++ utils/loss.py
@@ -6,6 +6,6 @@ class LabelSmoothingLoss(nn.Module):
         super(LabelSmoothingLoss, self).__init__()
 
     def forward(self, pred, label):
-        return torch.mean((-label * nn.LogSoftmax(dim=1)(pred)).sum(dim=0))
+        return torch.mean((-label * nn.LogSoftmax(dim=1)(pred)).mean(dim=0))
 
 
