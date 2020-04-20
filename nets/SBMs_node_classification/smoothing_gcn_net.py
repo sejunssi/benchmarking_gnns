daf01c94945e989ade483a55268c5a7de752a7db
sejunssi <skim3393@gmail.com>
2020-04-20 03:50:29 -0500

    fix

--- nets/SBMs_node_classification/smoothing_gcn_net.py
+++ nets/SBMs_node_classification/smoothing_gcn_net.py
@@ -81,7 +81,7 @@ class Smooth_GCNNet(nn.Module):
         w = self.MLP_layer(h2)
         w = torch.clamp(w, min=-delta, max=delta)
         ones = torch.ones(label.shape[0], label.shape[1]).to(device=self.device)
-        max_entropy = torch.Tensor([1/ len(label.shape[1])]).repeat(label.shape[0], label.shape[1]).to(device=self.device)
+        max_entropy = torch.Tensor([1/ label.shape[1]]).repeat(label.shape[0], label.shape[1]).to(device=self.device)
         g_hat = (ones-w) * label + w * max_entropy
         g_hat = self.softmax(g_hat)
         return p, g_hat
