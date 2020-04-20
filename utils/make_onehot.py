987247e55380847912d64e73f2fe4ac4d28cf9b9
sejunssi <skim3393@gmail.com>
2020-04-19 22:39:06 -0500

    fix make one hot

--- utils/make_onehot.py
+++ utils/make_onehot.py
@@ -1,9 +1,8 @@
 import torch
 
 def make_onehot_node_label(trainset, n_class):
+    result_node_labels=[]
     for i, data_list in enumerate(trainset.dataset):
-        trainset.dataset[i]['node_label'] = [torch.nn.functional.one_hot(data, n_class) for data in data_list['node_label']]
-    result_node_labels = []
-    for data_list in trainset.node_labels:
-        result_node_labels.append([torch.nn.functional.one_hot(data,n_class) for data in data_list])
+        trainset.dataset[i]['node_label'] = [torch.nn.functional.one_hot(data_list['node_label'], n_class)]
+        result_node_labels.append(trainset.dataset[i]['node_label'])
     trainset.node_labels = result_node_labels
