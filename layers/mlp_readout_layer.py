ad816866bcfb5e9d28755fc238ccd4e5bb18999a
sh92 <sanghee.lee1992@gmail.com>
2020-04-25 19:20:21 +0900

    fix MTL smoothing_gcn and delete unnessary config

--- layers/mlp_readout_layer.py
+++ layers/mlp_readout_layer.py
@@ -22,3 +22,35 @@ class MLPReadout(nn.Module):
             y = F.relu(y)
         y = self.FC_layers[self.L](y)
         return y
+
+
+class ResnetMLPReadout(nn.Module):
+
+    def __init__(self, input_dim, output_dim, L=2):  # L=nb_hidden_layers
+        super().__init__()
+        list_FC_layers = [nn.Linear(input_dim // 2 ** l, input_dim // 2 ** (l + 1), bias=True) for l in range(L)]
+        list_FC_layers.append(nn.Linear(input_dim // 2 ** L, output_dim, bias=True))
+        self.FC_layers = nn.ModuleList(list_FC_layers)
+        list_conv_layers = [self.conv1x1(input_dim, input_dim // 2 ** (l + 1)) for l in range(L)]
+        list_conv_layers.append(self.conv1x1(input_dim, input_dim // 2 ** L))
+        self.downsample_conv = nn.ModuleList(list_conv_layers)
+        list_bn_layers = [nn.BatchNorm2d(input_dim // 2 ** (l + 1)) for l in range(L)]
+        list_bn_layers.append(nn.BatchNorm2d(input_dim // 2 ** L))
+        self.downsample_bn = nn.ModuleList(list_bn_layers)
+        self.L = L
+
+    def conv1x1(self, in_planes, out_planes, stride=1):
+        """1x1 convolution"""
+        return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+    def forward(self, x):
+        y = x
+        for l in range(self.L):
+            y = self.FC_layers[l](y)
+            if self.downsample_conv is not None:
+                identity = self.downsample_conv[l](x.reshape((x.shape[0], x.shape[1], 1, 1)))
+                identity = self.downsample_bn[l](identity).squeeze()
+            y += identity
+            y = F.relu(y)
+        y = self.FC_layers[self.L](y)
+        return y
