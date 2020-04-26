04236fc6643a3b2409f1778d42515c65a47ab286
sejunssi <skim3393@gmail.com>
2020-04-25 21:34:54 -0500

    rename from classical to classic

--- generate_classic_label_smoothing_preprocess.py
+++ generate_classic_label_smoothing_preprocess.py
@@ -19,7 +19,7 @@ def make_onehot_data(data):
     data[0].node_labels = onehot_data_list
     return n_classes
 
-def classical_smoothing(w, data, n_classes):
+def classic_smoothing(w, data, n_classes):
     smoothed_label = (1-w) * data.to(torch.float) + torch.ones(data.shape[0], data.shape[1]).to(torch.float)/n_classes * w
     return smoothed_label
 
@@ -27,15 +27,15 @@ def classical_smoothing(w, data, n_classes):
 def make_label_smoothing(dataname, w, dataset, n_classes):
     dataset[0].node_labels = []
     for i, data in enumerate(dataset[0].dataset):
-        dataset[0].dataset[i]['node_label'] = classical_smoothing(w, data['node_label'], n_classes)
-        dataset[0].node_labels.append(classical_smoothing(w, data['node_label'], n_classes))
+        dataset[0].dataset[i]['node_label'] = classic_smoothing(w, data['node_label'], n_classes)
+        dataset[0].node_labels.append(classic_smoothing(w, data['node_label'], n_classes))
     w_str = str(w).split(".")[1]
     print(f"writing {dataname} w {w_str}")
     with open(f'data/SBMs/{dataname}'+'_'+f'w{w_str}.pkl', 'wb') as f:
         pickle.dump(dataset, f)
     print("Done")
 
-def classical_label_smoothing(DataSetName, w_list):
+def classic_label_smoothing(DataSetName, w_list):
     for dataname in DataSetName:
         for w in w_list:
             with open(f'data/SBMs/{dataname}.pkl', 'rb') as f:
@@ -45,4 +45,4 @@ def classical_label_smoothing(DataSetName, w_list):
 
 DataSetName = ['SBM_CLUSTER', 'SBM_PATTERN']
 w_list = [0.05, 0.1, 0.15, 0.2, 0.25]
-classical_label_smoothing(DataSetName, w_list)
+classic_label_smoothing(DataSetName, w_list)
